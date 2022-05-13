d3023f1980b20a0b5ca8024b0b6ce253364883fc
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-13 23:56:20 +0600

    validation fixed

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,47 +1,42 @@
-const registerFormValidation = () => {
-    const nameReg = /^[A-Za-z]+$/;
-    const mobileReg = /+?(88)?0?1[56789][0-9]{8}\b/;
-    const passReg = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+function registerFormValidation() {
+    const regPass = /.*[0-9].*/g;
+    const regaxName = /[a-zA-z._]/g;
+    const regaxNumber = /(\+88)?-?01[3-9][0-9]{8}/g;
+    const regEmail = /(cse|eee|cee|law|eng|arch)_\d{10}@lus\.ac\.bd/g;
 
-    let email = document.getElementById("email").value;
-    let fname = document.getElementById("fname").value;
-    let mobile = document.getElementById("mobile").value;
-    let pass = document.getElementById("pass").value;
-    let confirmPass = document.getElementById("confirm-pass").value;
-    if (!email.match(emailReg)) {
-        alert('Enter valid email');
+    const email = document.getElementById('email').value
+    if (email.match(regEmail)?.join('') != email) {
+        alert('Only LU email allowed')
     }
-    else if (!fname.match(nameReg)) {
-        alert('Your first name should not contain any digits');
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
+    const fullName = firstName + lastName;
+    if (fullName.match(regaxName).join('') != fullName) {
+        alert("Give Right Name")
     }
-    else if (!mobile.match(mobileReg)) {
-        alert('Only Bangladeshi operator is acceptable');
+    const number = document.getElementById('mobile').value;
+    if (number.match(regaxNumber)?.join('') != number) {
+        alert('Only BD number allowed')
     }
-    else if (!pass.match(passReg)) {
-        alert('Your password must contain a digit and it should be 10 character long');
+    const pass = document.getElementById('pass').value;
+    const pass1 = document.getElementById('pass').value;
+    if (pass == pass1) {
+        if (pass.match(regPass).join('') != pass) {
+            alert('Give right password')
+        }
     }
-    else if (pass !== confirmPass) {
-        alert('Please confirm your password');
+}
+function loginFormValidation() {
+    const regEmail = /(cse|eee|cee|law|eng|arch)_\d{10}@lus\.ac\.bd/g;
+    const regPass = /.*[0-9].*/g;
+
+    const email = document.getElementById('email').value
+    const pass = document.getElementById('pass').value;
+    if (email.match(regEmail)?.join('') != email) {
+        alert('Only LU email allowed')
     }
-    else {
-        alert('New Account Created');
+    else if (pass.match(regPass).join('') != pass) {
+        alert('Give right password')
     }
 }
 
-const loginFormValidation = () => {
-    const passReg = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    let email = document.getElementById("email").value;
-    let pass = document.getElementById("pass").value;
-    if (!email.match(emailReg)) {
-        alert('Enter valid email');
-    }
-    else if (!pass.match(passReg)) {
-        alert('Your password must contain a digit and it should be 10 character long');
-    }
-    else {
-        alert('Logged in');
-    }
-}
